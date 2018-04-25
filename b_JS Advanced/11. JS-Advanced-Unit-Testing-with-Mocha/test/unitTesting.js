0fc5688f70d86f4e2b5b607c041478ee809c2d46
Niki-home <nikmastera@gmail.com>
2018-04-25 20:00:13 +0300

    home

---
+++ b_JS Advanced/11. JS-Advanced-Unit-Testing-with-Mocha/test/unitTesting.js
@@ -0,0 +1,17 @@
+let expect = require('chai').expect
+
+function sum(arr) {
+    let sum = 0;
+    for (num of arr)
+        sum += Number(num);
+    return sum;
+}
+
+describe("sum(arr)", function() {
+    it("should return 3 for [1, 2]", function() {
+        expect(sum([1, 2])).to.be.equal(3);
+    });
+    it("should return 3 for [1, 2]", function() {
+        expect(sum([1, 2])).to.be.equal(4);
+    });
+});
